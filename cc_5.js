7b381ff366a3cd9e6ed938526adddf460b9e1c0b
Divya Joshi <divyaj.redj@gmail.com>
2025-02-04 18:08:50 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -3,3 +3,13 @@ let customer = {
 name: "John Doe", age: 35, email: "john.doe@email.com"
 }
 console.log(customer)
+
+//Task 2: Object Methods
+const order = {
+    orderId: 12345, totalAmount: 150, status: "Processing",
+    displayOrder() {
+        console.log(`Order ID: ${this.orderId}, Total Amount: ${this.totalAmount}, Status: ${this.status}`);
+      }
+};
+
+order.displayOrder()
